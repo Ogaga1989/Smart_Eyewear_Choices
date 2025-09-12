4500ea073d1ffc8a80850870a9d382fd03210531
Ogaga1989 <ogaga753@gmail.com>
2025-09-12 15:44:10 -0700

    Commit 7: Optimise framesdirect script to remove options such as window size
    The window-size option is removed because our driver is already using headless option

--- FrameDirect_Deliverables/framesdirect.py
+++ FrameDirect_Deliverables/framesdirect.py
@@ -61,8 +61,7 @@ else:
 # Setup Chrome WB options
 chrome_options = Options()
 chrome_options.add_argument("--headless")   
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--window-size=1920,1080")     
+chrome_options.add_argument("--disable-gpu")    
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.265 Safari/537.36")
 driver = webdriver.Chrome(options=chrome_options)
 print("done setting up..")
